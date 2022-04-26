afc2a1d5b979ed3514896cfcc8f343e386943c8b
Preksler <preksler@gmail.com>
2022-04-26 21:34:05 +0300

    finish task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const markup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+let instance = null;
+
+function createGalleryCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                            class="gallery__image"
+                            src="${preview}"
+                            data-source="${original}"
+                            alt="${description}"
+                        />
+                    </a>
+                </div>`;
+    }).join('');
+};
+
+function onGalleryContainerClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    window.addEventListener('keydown', onEscKeyPress);
+    instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}" alt="${event.target.alt}" width="800" height="600">
+    `);
+    instance.show();
+}
+
+function onEscKeyPress(event) {
+    if (event.code !== 'Escape') {
+        return;
+    }
+    instance.close();
+    window.removeEventListener('keydown', onEscKeyPress);
+}
